1dd6f1b6acbeb92d41e18b7f77f5092cc8f1b617
rcavakian <85318873+rcavakian@users.noreply.github.com>
2024-06-14 16:56:24 -0300

    Atualiza arquivos pubspec, cria arquivos models Tarefa e Responsavel

---
+++ lib/responsavel_api.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'consts.dart';
+
+class Responsavel {
+  int id;
+  final String nome;
+  final DateTime dataNascimento;
+
+  Responsavel({
+    required this.id,
+    required this.nome,
+    required this.dataNascimento
+  });
+
+  factory Responsavel.fromJson (Map<String, dynamic> json) => Responsavel (
+    id: json['id'],
+    nome: json['nome'],
+    dataNascimento: DateTime.parse(json['data_nascimento'])
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'nome': nome,
+    'data_nascimento': dataNascimento
+  };
+
+  Map<String, dynamic> toJsonForAdd() => {
+    'nome': nome,
+    'data_nascimento': dataNascimento
+  };
+}
+
+class ResponsavelProvider extends ChangeNotifier {
+  List<Responsavel> responsaveis = [];
+
+  Future<void> fetchResponsaveis() async {
+    const url = 'http://$localhost:3000/responsavel';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List<dynamic> responsavelData = data['responsaveis'];
+      responsaveis = responsavelData.map((item) => Responsavel.fromJson(item)).toList();
+      notifyListeners();
+    } else {
+      throw Exception('Failed to load responsáveis');
+    }
+  }
+
+  Future<void> addResponsavel(Responsavel responsavel) async {
+    const url = 'http://$localhost:3000/responsavel';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(responsavel.toJsonForAdd()),
+    );
+    if (response.statusCode != 200) {
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      throw Exception('Failed to add tarefa');
+    } else {
+      fetchResponsaveis();
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteResponsavel(int responsavelId) async {
+    final url = 'http://$localhost:3000/responsavel/$responsavelId';
+    final response = await http.delete(Uri.parse(url));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete responsavel');
+    } else {
+      responsaveis.removeWhere((tarefa) => tarefa.id == responsavelId);
+      notifyListeners();
+    }
+  }
+
+  Future<void> editResponsavel(int responsavelId, Responsavel updatedResponsavel) async {
+    final url = 'http://$localhost:3000/responsavel/$responsavelId';
+    final response = await http.put(
+      Uri.parse(url),
+      headers: {'Content-Type': "application/json"},
+      body: json.encode(updatedResponsavel.toJson()),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to edit responsavel');
+    } else {
+      final index = responsaveis.indexWhere((responsavel) => responsavel.id == responsavelId);
+      responsaveis[index] = updatedResponsavel;
+      notifyListeners();
+    }
+  }
+}
---
+++ lib/tarefa_api.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'consts.dart';
+
+
+
+
+class Tarefa {
+  int id;
+  final String titulo;
+  final String descricao;
+  final String responsavel;
+  final String status;
+  final DateTime dataLimite;
+
+  Tarefa({
+    required this.id,
+    required this.titulo,
+    required this.descricao,
+    required this.responsavel,
+    required this.status,
+    required this.dataLimite
+  });
+
+  factory Tarefa.fromJson (Map<String, dynamic> json) => Tarefa (
+    id: json['id'],
+    titulo: json['titulo'],
+    descricao: json['descricao'],
+    responsavel: json['responsavel'],
+    status: json['concluida'],
+    dataLimite: DateTime.parse(json['data_limite_conclusao'])
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'titulo': titulo,
+    'descricao': descricao,
+    'responsavel': responsavel,
+    'concluida': status,
+    'data_limite_conclusao': dataLimite.toIso8601String(),
+  };
+
+  Map<String, dynamic> toJsonForAdd() => {
+    'titulo': titulo,
+    'descricao': descricao,
+    'responsavel': responsavel,
+    'concluida': status,
+    'data_limite_conclusao': dataLimite.toIso8601String(),
+  };
+}
+
+class TarefaProvider extends ChangeNotifier {
+  List<Tarefa> tarefas = [];
+
+  Future<void> fetchTarefas() async {
+    const url = 'http://$localhost:3000/tarefa';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List<dynamic> tarefaData = data['tarefas'];
+      tarefas = tarefaData.map((item) => Tarefa.fromJson(item)).toList();
+      notifyListeners();
+    } else {
+      throw Exception('Failed to load tarefas');
+    }
+  }
+
+  Future<void> addTarefa(Tarefa tarefa) async {
+    const url = 'http://$localhost:3000/tarefa';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(tarefa.toJsonForAdd()),
+    );
+    if (response.statusCode != 200) {
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      throw Exception('Failed to add tarefa');
+    } else {
+      fetchTarefas();
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteTarefa(int tarefaId) async {
+    final url = 'http://$localhost:3000/tarefa/$tarefaId';
+    final response = await http.delete(Uri.parse(url));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete tarefa');
+    } else {
+      tarefas.removeWhere((tarefa) => tarefa.id == tarefaId);
+      notifyListeners();
+    }
+  }
+
+  Future<void> editTarefa(int tarefaId, Tarefa updatedTarefa) async {
+    final url = 'http://$localhost:3000/tarefa/$tarefaId';
+    final response = await http.put(
+      Uri.parse(url),
+      headers: {'Content-Type': "application/json"},
+      body: json.encode(updatedTarefa.toJson()),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to edit tarefa');
+    } else {
+      final index = tarefas.indexWhere((tarefa) => tarefa.id == tarefaId);
+      tarefas[index] = updatedTarefa;
+      notifyListeners();
+    }
+  }
+}
